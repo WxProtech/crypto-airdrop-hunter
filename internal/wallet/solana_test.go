f841df37577a9aedb6f4c69136bbd52ba6adc202
WxProtech <wz.protech@gmail.com>
2025-07-07 23:07:51 +0800

    fix: use index when deriving solana key

--- internal/wallet/solana_test.go
+++ internal/wallet/solana_test.go
@@ -6,10 +6,21 @@ import (
 
 func TestDeriveSolanaKeyPair(t *testing.T) {
 	mnemonic := "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about"
-	account, err := DeriveSolanaKeyPair(mnemonic, 0)
+
+	acc0, err := DeriveSolanaKeyPair(mnemonic, 0)
 	if err != nil {
 		t.Fatal("派生失败:", err)
 	}
-	t.Logf("Address: %s", account.PublicKey.ToBase58())
-	t.Logf("Private: %x", account.PrivateKey)
+
+	acc1, err := DeriveSolanaKeyPair(mnemonic, 1)
+	if err != nil {
+		t.Fatal("派生失败:", err)
+	}
+
+	if acc0.PublicKey.ToBase58() == acc1.PublicKey.ToBase58() {
+		t.Fatal("同一助记词在不同索引下应派生出不同地址")
+	}
+
+	t.Logf("Address0: %s", acc0.PublicKey.ToBase58())
+	t.Logf("Address1: %s", acc1.PublicKey.ToBase58())
 }
